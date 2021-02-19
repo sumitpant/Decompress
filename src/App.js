cbf0c9beb8eb8b5fc42a0fc47e38b76ef1e8a1eb
sumit <38173704+sumitpant@users.noreply.github.com>
2021-02-20 00:34:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React,{useEffect} from 'react'
 import './App.css';
+import json from './json.zip'
+import JSZipUtils from "jszip-utils"
+import JSZip from 'jszip';
 
 function App() {
+
+  useEffect(() => {
+                
+
+   const fun=async()=>{
+     
+
+      JSZipUtils.getBinaryContent(json,  async function(err, data) {
+        if(err) {
+            throw err; 
+        }
+    
+        let zip =JSZip(data);
+      
+        zip.loadAsync(data).then( async function (obj) {
+          
+          obj.forEach( async function  (relativePath, zipEntry) {  
+          
+             let result= await zip.file(zipEntry.name).async("string");
+             console.log(JSON.parse(result))
+           
+
+        });
+            
+        });
+    });
+
+   }
+    fun()
+    
+  
+
+
+  })
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+             HEllo
+           
+           
     </div>
   );
 }
